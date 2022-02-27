b9523c54a720a04aa7e1f4696cc216958b9b2612
badruzzaman <badruzzaman@DESKTOP-EHGT2P0>
2022-02-27 12:13:23 +0600

    new sources committed

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, abort
 
-app = Flask(__name__)
+app = Flask(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 @app.route('/')
 def index():
-    return 'Login page here!'
+    return render_template('index.html')
 
 @app.route('/dashboard')
 def dashboard():
-    return 'Hello dashboard!'
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run()
