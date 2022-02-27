2bfb7c48ef2c8cdfdeb6d237e143a08420e247d3
badruzzaman <badruzzaman@DESKTOP-EHGT2P0>
2022-02-27 15:40:32 +0600

    new files are added

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
